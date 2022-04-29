ff2aa910495526102d83b7cc2f68335c88a51ee0
diana <dianagonzalez4d@gmail.com>
2022-04-29 14:48:26 +0200

    Se eliminó código viejo

--- Web/cine-zone/src/app/interfaces/auth.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface AuthLoginResponse {
-    email: string;
-    nombre: string;
-    telefono: string;
-    fechaNacimiento: string;
-    token: string;
-}
--- Web/cine-zone/src/app/interfaces/dto/auth.dto.ts
+++
@@ -1,9 +0,0 @@
-export class AuthLoginDto{
-    email: string;
-    password: string;
-
-    constructor(){
-        this.email = '';
-        this.password = '';
-    }
-}

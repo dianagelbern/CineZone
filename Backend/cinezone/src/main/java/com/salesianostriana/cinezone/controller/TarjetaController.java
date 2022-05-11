effb73ac1e3bf83c8d3764357f9b7a453de0f6ca
diana <dianagonzalez4d@gmail.com>
2022-05-11 12:39:53 +0200

    Se añadió el método que trae todas las tarjetas asociadas a un usuario

--- Backend/cinezone/src/main/java/com/salesianostriana/cinezone/controller/TarjetaController.java
+++ Backend/cinezone/src/main/java/com/salesianostriana/cinezone/controller/TarjetaController.java
@@ -6,16 +6,24 @@ import com.salesianostriana.cinezone.dto.tarjetadto.TarjetaDtoConverter;
 import com.salesianostriana.cinezone.models.Tarjeta;
 import com.salesianostriana.cinezone.services.TarjetaService;
 import com.salesianostriana.cinezone.users.model.UserEntity;
+import com.salesianostriana.cinezone.util.PaginationLinkUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 
 @RestController
@@ -27,12 +35,41 @@ public class TarjetaController {
 
     private final TarjetaService tarjetaService;
     private final TarjetaDtoConverter tarjetaDtoConverter;
+    private final PaginationLinkUtils paginationLinkUtils;
 
-
+    @Operation(summary = "Crear una nueva tarjeta")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Se creó correctamente la tarjeta",
+                    content = { @Content(mediaType =  "application/json",
+                            schema = @Schema(implementation = UserEntity.class))}),
+            @ApiResponse(responseCode = "404",
+                    description = "No se pudo crear la tarjeta",
+                    content = @Content),
+    })
     @PostMapping("/")
     public ResponseEntity<GetTarjetaDto> createTarjeta(@RequestBody CreateTarjetaDto tarjetaDto, @AuthenticationPrincipal UserEntity currentUser){
         Tarjeta newTarjeta = tarjetaService.createTarjeta(tarjetaDto, currentUser);
         GetTarjetaDto getTarjetaDto = tarjetaDtoConverter.convertTarjetaToGetTarjetaDto(newTarjeta);
         return ResponseEntity.status(HttpStatus.CREATED).body(getTarjetaDto);
+    }
+
+    @Operation(summary = "Muestra todas las tarjetas de un usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se listan correctamente todas las tarjetas",
+                    content = { @Content(mediaType =  "application/json",
+                            schema = @Schema(implementation = UserEntity.class))}),
+            @ApiResponse(responseCode = "404",
+                    description = "La lista de tarjetas está vacía",
+                    content = @Content),
+    })
+    @GetMapping("/")
+    public ResponseEntity<Page<GetTarjetaDto>> findAllTarjetasByUser(@PageableDefault(size = 10, page = 0) Pageable pageable, HttpServletRequest request, @AuthenticationPrincipal UserEntity currentUser){
+        Page<Tarjeta> t = tarjetaService.findAllTarjetasByUser(pageable, currentUser);
+
+        Page<GetTarjetaDto> res = t.map(tarjetaDtoConverter::convertTarjetaToGetTarjetaDto);
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromHttpUrl(request.getRequestURL().toString());
+        return ResponseEntity.ok().header("link", paginationLinkUtils.createLinkHeader(res, uriComponentsBuilder)).body(res);
     }
 }
--- Backend/cinezone/src/main/java/com/salesianostriana/cinezone/repos/TarjetaRepository.java
+++ Backend/cinezone/src/main/java/com/salesianostriana/cinezone/repos/TarjetaRepository.java
@@ -1,7 +1,20 @@
 package com.salesianostriana.cinezone.repos;
 
 import com.salesianostriana.cinezone.models.Tarjeta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
 
 public interface TarjetaRepository extends JpaRepository<Tarjeta, Long> {
+
+    @Query(value = """
+            SELECT *
+            FROM TARJETA t
+            WHERE t.usuario_id = :id
+            """, nativeQuery = true)
+    Page<Tarjeta> findAllTarjetasByUser(@Param("id")UUID id, Pageable pageable);
 }

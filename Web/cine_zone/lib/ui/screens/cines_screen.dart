783178c025be93cb5cc1877472b9bd1898665102
diana <dianagonzalez4d@gmail.com>
2022-05-21 00:20:25 +0200

    Se añadió parte de la maquetación

---
+++ Web/cine_zone/lib/ui/screens/cines_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CinesScreen extends StatefulWidget {
+  const CinesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CinesScreen> createState() => _CinesScreenState();
+}
+
+class _CinesScreenState extends State<CinesScreen> {
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _opciones(),
+    );
+  }
+
+  Widget _opciones() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            child: Text(
+              "Cines",
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+          Container(
+            child: Row(
+              children: [_search(), _boton()],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _search() {
+    return Container(
+      height: 47,
+      width: 368,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color(0xFF2F2C44),
+      ),
+      child: TextFormField(
+        style: TextStyle(color: Colors.white),
+        controller: searchController,
+        decoration: const InputDecoration(
+          hintText: 'Buscar cine',
+          hintStyle: TextStyle(
+              fontSize: 13, color: Color.fromARGB(125, 255, 255, 255)),
+          focusedBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(244, 134, 122, 210))),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          )),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Color.fromARGB(55, 255, 255, 255),
+          ),
+        ),
+        onSaved: (String? value) {},
+        validator: (value) {
+          return (value == null || value.isEmpty)
+              ? 'Escribe el nombre del cine'
+              : null;
+        },
+      ),
+    );
+  }
+
+  Widget _boton() {
+    return Container(
+      margin: EdgeInsets.only(left: 60),
+      width: 161,
+      height: 47,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        gradient: new LinearGradient(
+          colors: [
+            Color.fromARGB(244, 134, 122, 210),
+            Color.fromARGB(255, 107, 97, 175)
+          ],
+          begin: FractionalOffset.topCenter,
+          end: FractionalOffset.bottomCenter,
+        ),
+      ),
+      child: TextButton(
+        onPressed: () {},
+        child: Text(
+          'Añadir cine',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}

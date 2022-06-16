840e7d0123d61e1e029dbfac23bce5efc30dead5
diana <dianagonzalez4d@gmail.com>
2022-06-16 21:14:21 +0200

    Se cambió el nombre de la constante del api base url por la del dominio

--- Web/cine_zone/lib/repository/constants.dart
+++ Web/cine_zone/lib/repository/constants.dart
@@ -1,5 +1,7 @@
 class Constant {
-  static final apiBaseUrl = 'http://localhost:8080';
+  //https://cinezone.herokuapp.com
+  static final apiBaseUrl = 'https://cinezone.herokuapp.com';
+  //static final apiBaseUrl = 'http://localhost:8080';
   //static final apiBaseUrl = 'http://192.168.1.124:8080';
   static final bearerToken = 'token';
 }
